19d5e0bb92354bc5c0f49eb93b162471775f718d
Amina Muumin <aminamu7@gmail.com>
2022-09-20 11:21:38 -0500

    first part done

--- client.js
+++ client.js
@@ -43,15 +43,78 @@ console.log('array of employee data: ',  employees );
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+// * The `name` property should contain the employee's name.
+// * The `bonusPercentage` property should contain the bonus percentage the employee is to receive. See section below for calculation instructions.
+// * The `totalCompensation` property should be the adjusted annual compensation (base annual + bonus)
+// * The `totalBonus` should be the employee's total bonus rounded to the nearest dollar.
 
-
-
+// Those who have a rating of a 2 or below should not receive a bonus.
+// - Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
+// - Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
+// - Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
+// - If their employee number is 4 digits long, this means they have been with the company for longer than 15 years,
+// and should receive an additional 5%.
+// - However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
+// - No bonus can be above 13% or below 0% total.
 // This function will calculate 1 employee's bonus!
 //
-function calculateIndividualEmployeeBonus( employee ) {  
+
+function calculateIndividualEmployeeBonus( person, array ) {  
+    let bonus = 0;
+    
+  for (let employed of array){
+    if (employed.name === person){
+
+      if (employed.reviewRating <= 2){
+        bonus = 0;
+      }
+      else if (employed.reviewRating === 3){
+        bonus = employed.annualSalary * (0.04 );
+      }
+      else if (employed.reviewRating === 4){
+        bonus = employed.annualSalary * (0.06);
+  
+        }
+      else if (employed.reviewRating === 5){
+         bonus = employed.annualSalary * (0.1);
+    
+         }
+         
+        }
+      }
+       return bonus;
+      }
+
+
+  console.log(calculateIndividualEmployeeBonus('Jem', employees));
+
+
+
+      // if (employed.reviewRating <= 2){
+      //   bonus = 0
+      // }
+      // // return money;
+      // if (employed.reviewRating == 3){
+      // bonus = employed.annualSalary * (0.04 )
+      // }
+
+      // if (employed.reviewRating == 4){
+      // bonus = employed.annualSalary * (0.06)
+
+      // }
+
+      // if (employed.reviewRating == 5){
+      // bonus = employed.annualSalary * (0.1)
+
+      // }
+      
+      // return bonus;
+      
+  
+
+  
   // your logic here
   
   
   // return new object with bonus results
 
-}
